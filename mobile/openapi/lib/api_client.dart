19dd8190ef2787124edd566f1026db4624f8f29f
Pierre Marais <pierremarais67@gmail.com>
2026-05-11 23:19:26 +0200

    chore: regenerate mobile openapi client

--- mobile/openapi/lib/api_client.dart
+++ mobile/openapi/lib/api_client.dart
@@ -581,6 +581,8 @@ class ApiClient {
           return QueueJobResponseDto.fromJson(value);
         case 'QueueJobStatus':
           return QueueJobStatusTypeTransformer().decode(value);
+        case 'QueueJobTypeCountsDto':
+          return QueueJobTypeCountsDto.fromJson(value);
         case 'QueueName':
           return QueueNameTypeTransformer().decode(value);
         case 'QueueResponseDto':
