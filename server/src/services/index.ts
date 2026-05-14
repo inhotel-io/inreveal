d720b033927d9de9006cb9b7ef5b825d8cb5a327
Pierre Marais <pierremarais67@gmail.com>
2026-05-14 14:10:03 +0200

    feat: add encrypted agent secret service

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -13,6 +13,7 @@ import { DatabaseBackupService } from 'src/services/database-backup.service';
 import { DatabaseService } from 'src/services/database.service';
 import { DownloadService } from 'src/services/download.service';
 import { DuplicateService } from 'src/services/duplicate.service';
+import { EncryptedSecretService } from 'src/services/encrypted-secret.service';
 import { FaceRepairService } from 'src/services/face-repair.service';
 import { HlsService } from 'src/services/hls.service';
 import { IntegrityService } from 'src/services/integrity.service';
@@ -73,6 +74,7 @@ export const services = [
   DatabaseService,
   DownloadService,
   DuplicateService,
+  EncryptedSecretService,
   FaceRepairService,
   IntegrityService,
   HlsService,
