618000e8da548d96efeadfba047451e5ba9176d2
Pierre Marais <pierremarais67@gmail.com>
2026-05-16 10:43:12 +0200

    feat(server): add PersonSuggestionScan job names + payload types

--- server/src/enum.ts
+++ server/src/enum.ts
@@ -919,6 +919,8 @@ export enum JobName {
   FaceIdentityBackfill = 'FaceIdentityBackfill',
   FaceIdentityMaintenanceAfterRecognition = 'FaceIdentityMaintenanceAfterRecognition',
   FaceRepairScan = 'FaceRepairScan',
+  PersonSuggestionScanQueueAll = 'PersonSuggestionScanQueueAll',
+  PersonSuggestionScan = 'PersonSuggestionScan',
 
   FileDelete = 'FileDelete',
   FileMigrationQueueAll = 'FileMigrationQueueAll',
