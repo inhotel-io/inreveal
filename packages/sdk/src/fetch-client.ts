9dce243cb5a411e36d28a91f76382e9d586f2613
Pierre Marais <pierremarais67@gmail.com>
2026-05-16 09:53:32 +0200

    chore(generated): regenerate OpenAPI clients for SharedSpaceFaceMatchFromBackfill

--- packages/sdk/src/fetch-client.ts
+++ packages/sdk/src/fetch-client.ts
@@ -9163,6 +9163,7 @@ export enum JobName {
     SharedSpaceFaceMatch = "SharedSpaceFaceMatch",
     SharedSpaceFaceMatchAll = "SharedSpaceFaceMatchAll",
     SharedSpaceFaceMatchPage = "SharedSpaceFaceMatchPage",
+    SharedSpaceFaceMatchFromBackfill = "SharedSpaceFaceMatchFromBackfill",
     SharedSpaceLibraryFaceSync = "SharedSpaceLibraryFaceSync",
     SharedSpaceIdentityReconciliation = "SharedSpaceIdentityReconciliation",
     SharedSpacePersonDedup = "SharedSpacePersonDedup",
