2c206e54dd9633fb4c84f499f7ae35b6631fccd6
Pierre Marais <pierremarais67@gmail.com>
2026-05-16 10:43:12 +0200

    feat(server): add PersonSuggestionScan job names + payload types

--- server/src/types.ts
+++ server/src/types.ts
@@ -296,6 +296,10 @@ export interface IFaceIdentityBackfillJob extends IBaseJob {
   continuationCount?: number;
 }
 
+export interface IPersonSuggestionScanJob extends IBaseJob {
+  id: string;
+}
+
 export interface ISharedSpaceFaceMatchJob extends IBaseJob {
   spaceId: string;
   assetId: string;
@@ -515,6 +519,8 @@ export type JobItem =
   | { name: JobName.FacialRecognition; data: IFacialRecognitionJob }
   | { name: JobName.FaceIdentityBackfill; data: IFaceIdentityBackfillJob }
   | { name: JobName.FaceIdentityMaintenanceAfterRecognition; data: IDelayedJob }
+  | { name: JobName.PersonSuggestionScanQueueAll; data: IBaseJob }
+  | { name: JobName.PersonSuggestionScan; data: IPersonSuggestionScanJob }
   | { name: JobName.PersonGenerateThumbnail; data: IEntityJob }
 
   // Smart Search
