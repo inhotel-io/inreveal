be8254a48bcad049ba75ec76bd8f0dd1dde82f1f
Pierre Marais <pierremarais67@gmail.com>
2026-05-25 18:36:40 -0400

    feat: add pi agent metadata operation contract

--- server/src/enum.ts
+++ server/src/enum.ts
@@ -173,6 +173,7 @@ export enum AgentOperationType {
   AssetRotate = 'asset.rotate',
   AssetSetFavorite = 'asset.setFavorite',
   AssetSetArchive = 'asset.setArchive',
+  AssetUpdateMetadata = 'asset.updateMetadata',
   AssetAddTag = 'asset.addTag',
   AssetRemoveTag = 'asset.removeTag',
 }
