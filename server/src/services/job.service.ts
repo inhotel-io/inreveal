dfa1b31b46cc5397b0fd52b7ea3ac5d6d8be48c5
Pierre Marais <pierremarais67@gmail.com>
2026-05-31 14:19:12 -0400

    feat(server): auto-score image quality on upload fanout (A3 review fix)

--- server/src/services/job.service.ts
+++ server/src/services/job.service.ts
@@ -154,6 +154,7 @@ export class JobService extends BaseService {
           { name: JobName.AssetDetectFaces, data: item.data },
           { name: JobName.Ocr, data: item.data },
           { name: JobName.PetDetection, data: item.data },
+          { name: JobName.ImageQuality, data: item.data },
         ];
 
         if (asset.type === AssetType.Video) {
