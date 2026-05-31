751d88475074ca1b6d876c89d0d99a11cf692d7c
Pierre Marais <pierremarais67@gmail.com>
2026-05-30 22:28:22 -0400

    refactor(mobile): remove unused DriftSearchPage; route similar-photos to live search

--- mobile/lib/routing/router.gr.dart
+++ mobile/lib/routing/router.gr.dart
@@ -1064,22 +1064,6 @@ class DriftRecentlyTakenRoute extends PageRouteInfo<void> {
 }
 
 /// generated route for
-/// [DriftSearchPage]
-class DriftSearchRoute extends PageRouteInfo<void> {
-  const DriftSearchRoute({List<PageRouteInfo>? children})
-    : super(DriftSearchRoute.name, initialChildren: children);
-
-  static const String name = 'DriftSearchRoute';
-
-  static PageInfo page = PageInfo(
-    name,
-    builder: (data) {
-      return const DriftSearchPage();
-    },
-  );
-}
-
-/// generated route for
 /// [DriftTrashPage]
 class DriftTrashRoute extends PageRouteInfo<void> {
   const DriftTrashRoute({List<PageRouteInfo>? children})
