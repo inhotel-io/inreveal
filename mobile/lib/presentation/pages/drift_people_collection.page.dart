CarWalTech/gallery

feat: user-controlled People view sort on web and mobile (#683)
* docs: spec for People view sort option (#676)

* docs: people-sort spec — TDD process, edge-case matrix, picker isolation, trim fix

* docs: interactive web+mobile mockup for people-sort spec (#676)

* docs: slice-1 (web) implementation plan for people sort (#676)

* feat(web): add PeopleSortBy preference and parameterized people comparator (#676)

* feat(web): user-controlled people sort with Most-photos default (#676)

* feat(web): people sort preference on the spaces people page (#676)

* docs: slice-2 (mobile) implementation plan for people sort (#676)

* feat(mobile): PeopleSortBy setting with photoCount default (#676)

* feat(mobile): two-mode people ordering in DriftPeopleRepository (#676)

* feat(mobile): sort-keyed people provider family with pinned picker and library card (#676)

* feat(mobile): people sort menu button (#676)

* feat(mobile): people view sort control with persisted preference (#676)

* fix(mobile): people sort verification fixes (#676)

* fix: align web Name-mode tiebreak with mobile and satisfy format gates (#676)

mobile/lib/presentation/pages/drift_people_collection.page.dart
@@ -3,8 +3,12 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
+import 'package:immich_mobile/domain/models/person.model.dart';
+import 'package:immich_mobile/domain/models/setting.model.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
+import 'package:immich_mobile/providers/infrastructure/setting.provider.dart';
 import 'package:immich_mobile/presentation/widgets/images/remote_image_provider.dart';
+import 'package:immich_mobile/presentation/widgets/people/people_sort_button.widget.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/utils/image_url_builder.dart';
 import 'package:immich_mobile/utils/people.utils.dart';
@@ -30,7 +34,10 @@ class _DriftPeopleCollectionPageState extends ConsumerState<DriftPeopleCollectio
 
   @override
   Widget build(BuildContext context) {
-    final people = ref.watch(driftGetAllPeopleProvider);
+    final sortBy = ref.watch(
+      settingsProvider.select((settings) => peopleSortByFromSettingIndex(settings.get(Setting.peopleSortBy))),
+    );
+    final people = ref.watch(driftGetAllPeopleProvider(sortBy));
 
     return LayoutBuilder(
       builder: (context, constraints) {
@@ -51,6 +58,7 @@ class _DriftPeopleCollectionPageState extends ConsumerState<DriftPeopleCollectio
                   )
                 : Text('people'.tr()),
             actions: [
+              const PeopleSortButton(),
               IconButton(
                 icon: Icon(_search != null ? Icons.close : Icons.search),
                 onPressed: () {

inhotel-io/inreveal

mobile/lib/presentation/pages/drift_people_collection.page.dart
@@ -7,7 +7,9 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/extensions/build_context_extensions.dart';
 import 'package:immich_mobile/extensions/string_extensions.dart';
 import 'package:immich_mobile/presentation/widgets/images/remote_image_provider.dart';
+import 'package:immich_mobile/presentation/widgets/people/people_sort_button.widget.dart';
 import 'package:immich_mobile/providers/infrastructure/people.provider.dart';
+import 'package:immich_mobile/providers/infrastructure/settings.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:immich_mobile/utils/image_url_builder.dart';
 import 'package:immich_mobile/utils/people.utils.dart';
@@ -33,7 +35,8 @@ class _DriftPeopleCollectionPageState extends ConsumerState<DriftPeopleCollectio
 
   @override
   Widget build(BuildContext context) {
-    final people = ref.watch(driftGetAllPeopleProvider);
+    final sortBy = ref.watch(appConfigProvider.select((config) => config.people.sortBy));
+    final people = ref.watch(driftGetAllPeopleProvider(sortBy));
 
     return LayoutBuilder(
       builder: (context, constraints) {
@@ -54,6 +57,7 @@ class _DriftPeopleCollectionPageState extends ConsumerState<DriftPeopleCollectio
                   )
                 : Text('people'.tr()),
             actions: [
+              const PeopleSortButton(),
               IconButton(
                 icon: Icon(_search != null ? Icons.close : Icons.search),
                 onPressed: () {