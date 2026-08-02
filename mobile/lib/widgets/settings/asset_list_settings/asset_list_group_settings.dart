7d0c7109fc788d43880b2eac7433c84b5348485f
Pierre Marais <pierremarais67@gmail.com>
2026-08-02 11:15:38 +0200

    refactor(mobile): read the grid grouping setting through one provider
    GroupSettings duplicated the normalizeGridGrouping(appConfigProvider...)
    rule that timelineGridGroupingProvider already encapsulates. Watch the
    provider instead so the setting's meaning is defined in one place.
    Behaviour-preserving.

--- mobile/lib/widgets/settings/asset_list_settings/asset_list_group_settings.dart
+++ mobile/lib/widgets/settings/asset_list_settings/asset_list_group_settings.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:immich_mobile/domain/models/timeline.model.dart';
-import 'package:immich_mobile/domain/models/timeline_grouping.model.dart';
 import 'package:immich_mobile/extensions/translate_extensions.dart';
 import 'package:immich_mobile/providers/app_settings.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/settings.provider.dart';
 import 'package:immich_mobile/providers/infrastructure/timeline.provider.dart';
+import 'package:immich_mobile/providers/timeline/timeline_grouping.provider.dart';
 import 'package:immich_mobile/widgets/settings/setting_group_title.dart';
 import 'package:immich_mobile/widgets/settings/settings_radio_list_tile.dart';
 
@@ -17,9 +17,7 @@ class GroupSettings extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final groupBy = useValueNotifier(
-      normalizeGridGrouping(ref.watch(appConfigProvider.select((s) => s.timeline.groupAssetsBy))),
-    );
+    final groupBy = useValueNotifier(ref.watch(timelineGridGroupingProvider));
 
     Future<void> updateAppSettings(GroupAssetsBy groupBy) async {
       await ref.read(settingsProvider).write(.timelineGroupAssetsBy, groupBy);
