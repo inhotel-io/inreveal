BierDav/noodle-gallery

feat(mobile): view a space's own people from inside a space (#980)
* docs(mobile): spec the space-scoped People page

Adds a design for viewing a shared space's own people from inside the
space on mobile, matching the web space People tab.

The feature is mobile-client-only: the server endpoint, the Dart SDK
bindings, the space-aware write routing and every i18n key already
exist. What is missing is the list fetch, the page and the entry point.

Specifies BDD acceptance criteria (38 scenarios) with a TDD slice
order, and records the traps the implementation has to survive:
Optional.value throwing on absent fields, paging termination without a
hasNextPage envelope, the reactive editability the PeopleGrid
extraction must preserve, and the absence of any correct offline
fallback for people who have no local Drift rows.

* docs(mobile): implementation plan for the space-scoped People page

Seven tasks, each a full RED-GREEN-commit cycle with the real test and
implementation code inline. Covers all 38 spec scenarios.

Also refines the spec: B27 is asserted on the PeopleGrid callback rather
than through a router harness, which would have to build the whole
DriftPersonPage and its timeline providers just to observe a push.

* docs(mobile): fix four defects found reviewing the plan

- The PeopleGrid extraction renamed the CircleAvatar key, which
  drift_people_collection_test.dart:168 resolves the avatar by. That
  would have tripped the plan's own must-pass-unmodified guard.
- B26 was claimed but never asserted: the sort test only proved the
  search filter survived a re-sort, not that the order changed.
- The max-page guard test allocated 100k DTOs; pageSize and maxPages
  are now defaulted test seams.
- The LayoutBuilder move was described as verbatim when it changes the
  measured width by the horizontal safe-area insets.

* docs(mobile): drop an unused import from the plan's Task 5 code

Caught in the pre-flight scan: every type person.model.dart would have
provided th

mobile/lib/repositories/person_api.repository.dart
@@ -3,6 +3,7 @@ import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/providers/api.provider.dart';
 import 'package:immich_mobile/repositories/api.repository.dart';
 import 'package:immich_mobile/services/api.service.dart';
+import 'package:immich_mobile/utils/people_sort.dart';
 import 'package:openapi/api.dart';
 
 final personApiRepositoryProvider = Provider((ref) => PersonApiRepository(ref.watch(apiServiceProvider)));
@@ -44,48 +45,11 @@ class PersonApiRepository extends ApiRepository {
       page += 1;
     }
     // The server returns identity-projected people in name order; re-sort client-side to
-    // honour the People page's sort setting, exactly like the web page (sortPeople).
-    dtos.sort((a, b) => _comparePeople(a, b, sortBy));
-    return dtos.map(_personToDriftPerson).toList();
-  }
-
-  // Mirrors the web comparePeople / the local Drift ORDER BY: favorites first, named before
-  // unnamed, then name/asset-count depending on the sort mode, with id as the tiebreaker.
-  // Hidden people are excluded server-side (withHidden:false), so isHidden is not compared.
-  static int _comparePeople(PersonResponseDto a, PersonResponseDto b, PeopleSortBy sortBy) {
-    final aFavorite = a.isFavorite.orElse(null) ?? false;
-    final bFavorite = b.isFavorite.orElse(null) ?? false;
-    if (aFavorite != bFavorite) {
-      return aFavorite ? -1 : 1;
-    }
-
-    final aName = a.name.trim();
-    final bName = b.name.trim();
-    final aHasName = aName.isNotEmpty;
-    final bHasName = bName.isNotEmpty;
-    if (aHasName != bHasName) {
-      return aHasName ? -1 : 1;
-    }
-
-    final nameCompare = aName.toLowerCase().compareTo(bName.toLowerCase());
-    // Most assets first.
-    final countCompare = (b.numberOfAssets.orElse(null) ?? 0).compareTo(a.numberOfAssets.orElse(null) ?? 0);
-    if (aHasName && sortBy == PeopleSortBy.name) {
-      if (nameCompare != 0) {
-        return nameCompare;
-      }
-      if (countCompare != 0) {
-        return countCompare;
-      }
-    } else {
-      if (countCompare != 0) {
-        return countCompare;
-      }
-      if (nameCompare != 0) {
-        return nameCompare;
-      }
-    }
-    return a.id.compareTo(b.id);
+    // honour the People page's sort setting, exactly like the web page (sortPeople). Mapping
+    // first lets both repositories share one comparator over DriftPerson.
+    final people = dtos.map(_personToDriftPerson).toList();
+    people.sort((a, b) => comparePeople(a, b, sortBy));
+    return people;
   }
 
   static DriftPerson _personToDriftPerson(PersonResponseDto dto) {

inhotel-io/inreveal

mobile/lib/repositories/person_api.repository.dart
@@ -3,6 +3,7 @@ import 'package:immich_mobile/domain/models/person.model.dart';
 import 'package:immich_mobile/providers/api.provider.dart';
 import 'package:immich_mobile/repositories/api.repository.dart';
 import 'package:immich_mobile/services/api.service.dart';
+import 'package:immich_mobile/utils/people_sort.dart';
 import 'package:openapi/api.dart';
 
 final personApiRepositoryProvider = Provider((ref) => PersonApiRepository(ref.watch(apiServiceProvider)));
@@ -39,48 +40,11 @@ class PersonApiRepository extends ApiRepository {
       page += 1;
     }
     // The server returns identity-projected people in name order; re-sort client-side to
-    // honour the People page's sort setting, exactly like the web page (sortPeople).
-    dtos.sort((a, b) => _comparePeople(a, b, sortBy));
-    return dtos.map(_toPerson).toList();
-  }
-
-  // Mirrors the web comparePeople / the local Drift ORDER BY: favorites first, named before
-  // unnamed, then name/asset-count depending on the sort mode, with id as the tiebreaker.
-  // Hidden people are excluded server-side (withHidden:false), so isHidden is not compared.
-  static int _comparePeople(PersonResponseDto a, PersonResponseDto b, PeopleSortBy sortBy) {
-    final aFavorite = a.isFavorite.orElse(null) ?? false;
-    final bFavorite = b.isFavorite.orElse(null) ?? false;
-    if (aFavorite != bFavorite) {
-      return aFavorite ? -1 : 1;
-    }
-
-    final aName = a.name.trim();
-    final bName = b.name.trim();
-    final aHasName = aName.isNotEmpty;
-    final bHasName = bName.isNotEmpty;
-    if (aHasName != bHasName) {
-      return aHasName ? -1 : 1;
-    }
-
-    final nameCompare = aName.toLowerCase().compareTo(bName.toLowerCase());
-    // Most assets first.
-    final countCompare = (b.numberOfAssets.orElse(null) ?? 0).compareTo(a.numberOfAssets.orElse(null) ?? 0);
-    if (aHasName && sortBy == PeopleSortBy.name) {
-      if (nameCompare != 0) {
-        return nameCompare;
-      }
-      if (countCompare != 0) {
-        return countCompare;
-      }
-    } else {
-      if (countCompare != 0) {
-        return countCompare;
-      }
-      if (nameCompare != 0) {
-        return nameCompare;
-      }
-    }
-    return a.id.compareTo(b.id);
+    // honour the People page's sort setting, exactly like the web page (sortPeople). Mapping
+    // first lets both repositories share one comparator over Person.
+    final people = dtos.map(_toPerson).toList();
+    people.sort((a, b) => comparePeople(a, b, sortBy));
+    return people;
   }
 
   // The unified Person model carries no owner/created/face-asset/hidden/colour fields, and