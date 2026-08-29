BierDav/noodle-gallery

fix(mobile): land memory "view in timeline" on the actual photo (#822) (#886)
* docs: design for memory jump-to-photo timeline navigation (#822)

* docs: harden #822 spec after codebase review

- derive overview state from rendered segments, not timelineGroupingProvider
  (args override, dateless sources and route scopes all desync from it)
- specify the async resolution contract: re-entrancy, unmount and staleness
- increment the attempt budget on switchToDayGrouping so it cannot spin
- cap the chunk scan to bound TimelineService buffer eviction
- add missing edge cases and an explicit red-green TDD order

* docs: implementation plan for memory jump-to-photo (#822)

* docs: fix #822 plan gaps found in codebase review

- target the memory asset actually on screen, not memory.assets.first
- move overview detection onto a Segment.isOverview getter
- correct a duplicate-import instruction and a stuck isScrubbing path

* feat(mobile): detect overview timeline rendering from built segments

* docs: scope the #822 plan's format step to touched files only

mobile/mise.toml gates `dart format` over lib/ only; running it over test/
with the pinned 3.44.8 reflows ~30 unrelated files into the diff.

* feat(mobile): drill overview timelines to day for view-in-timeline (#822)

* feat(mobile): add bounded chunk windowing for timeline asset scans

* feat(mobile): resolve an asset's absolute timeline index by bounded scan

* feat(mobile): compute the scroll offset of an asset's row in a segment

* feat(mobile): add the async scroll-resolution guard decision

* refactor(mobile): latch the asset rather than the date for view-in-timeline

* feat(mobile): briefly highlight the tile a timeline jump lands on

* test(mobile): make the highlight-clear test actually catch a missing cancel

The prior version asserted state==null both immediately after clear() and
after elapsing time, but clear() and the timer's own callback both only ever
set state to null -- deleting the timer cancellation 

mobile/lib/presentation/widgets/memory/memory_bottom_info.widget.dart
@@ -3,19 +3,27 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
-import 'package:immich_mobile/providers/asset_viewer/scroll_to_date_notifier.provider.dart';
+import 'package:immich_mobile/providers/asset_viewer/scroll_to_asset_notifier.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 
+/// The asset shown on [page] of [memory], clamped to the memory's bounds.
+///
+/// `currentAssetPage` in the memory page belongs to the ACTIVE memory, so an
+/// inactive page in the vertical PageView can ask for an index this memory does
+/// not have.
+RemoteAsset memoryAssetForPage(DriftMemory memory, int page) => memory.assets[page.clamp(0, memory.assets.length - 1)];
+
 class DriftMemoryBottomInfo extends StatelessWidget {
-  final DriftMemory memory;
+  final RemoteAsset asset;
   final String title;
-  const DriftMemoryBottomInfo({super.key, required this.memory, required this.title});
+  const DriftMemoryBottomInfo({super.key, required this.asset, required this.title});
 
   @override
   Widget build(BuildContext context) {
     final df = DateFormat.yMMMMd();
-    final fileCreatedDate = memory.assets.first.createdAt;
+    final fileCreatedDate = asset.createdAt;
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Row(
@@ -43,8 +51,8 @@ class DriftMemoryBottomInfo extends StatelessWidget {
                 // Activate the existing timeline tab without rebuilding it (a fresh
                 // TabShellRoute would reload the timeline to the top and discard the scroll).
                 await context.navigateTo(const MainTimelineRoute());
-                // #28941: scroll to the date in the viewer's local time, not UTC.
-                scrollToDateNotifierProvider.scrollToDate(fileCreatedDate.toLocal());
+                // #28941: the notifier converts to the viewer's local time itself.
+                scrollToAssetNotifierProvider.scrollToAsset(asset);
               },
               shape: const CircleBorder(),
               color: Colors.white.withValues(alpha: 0.2),

inhotel-io/inreveal

mobile/lib/presentation/widgets/memory/memory_bottom_info.widget.dart
@@ -1,20 +1,28 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:immich_mobile/domain/models/asset/base_asset.model.dart';
 import 'package:immich_mobile/domain/models/memory.model.dart';
 import 'package:immich_mobile/generated/translations.g.dart';
-import 'package:immich_mobile/providers/asset_viewer/scroll_to_date_notifier.provider.dart';
+import 'package:immich_mobile/providers/asset_viewer/scroll_to_asset_notifier.provider.dart';
 import 'package:immich_mobile/routing/router.dart';
 import 'package:intl/intl.dart';
 
+/// The asset shown on [page] of [memory], clamped to the memory's bounds.
+///
+/// `currentAssetPage` in the memory page belongs to the ACTIVE memory, so an
+/// inactive page in the vertical PageView can ask for an index this memory does
+/// not have.
+RemoteAsset memoryAssetForPage(Memory memory, int page) => memory.assets[page.clamp(0, memory.assets.length - 1)];
+
 class MemoryBottomInfo extends StatelessWidget {
-  final Memory memory;
+  final RemoteAsset asset;
   final String title;
-  const MemoryBottomInfo({super.key, required this.memory, required this.title});
+  const MemoryBottomInfo({super.key, required this.asset, required this.title});
 
   @override
   Widget build(BuildContext context) {
     final df = DateFormat.yMMMMd();
-    final fileCreatedDate = memory.assets.first.createdAt;
+    final fileCreatedDate = asset.createdAt;
     return Padding(
       padding: const EdgeInsets.all(16.0),
       child: Row(
@@ -46,8 +54,8 @@ class MemoryBottomInfo extends StatelessWidget {
                 // Activate the existing timeline tab without rebuilding it (a fresh
                 // TabShellRoute would reload the timeline to the top and discard the scroll).
                 await context.navigateTo(const MainTimelineRoute());
-                // #28941: scroll to the date in the viewer's local time, not UTC.
-                scrollToDateNotifierProvider.scrollToDate(fileCreatedDate.toLocal());
+                // #28941: the notifier converts to the viewer's local time itself.
+                scrollToAssetNotifierProvider.scrollToAsset(asset);
               },
               shape: const CircleBorder(),
               color: Colors.white.withValues(alpha: 0.2),